93d45f42df2473e9970108e4034dac5eb960616e
Nikolaos Agas <nagas@vanderbox.gr>
2017-11-06 15:04:53 +0200

    Initial commit

---
+++ app/build/generated/source/dataBinding/legacy/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="17bef806-8d51-4823-a626-418d10d15762")
+public class DataBindingInfo {}
